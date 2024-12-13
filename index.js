cacaeb0d9fb6ccd3caad302f5cab403949f8d74f
CodyBense <codybense@gmail.com>
2024-12-13 18:18:14 -0500

    added help and playlist command

--- index.js
+++ index.js
@@ -11,12 +11,30 @@ client.once(Events.ClientReady, c => {
         .setName("ping")
         .setDescription("Replies with ping!");
 
+    const help = new SlashCommandBuilder()
+        .setName("help")
+        .setDescription("Replies with list of commands");
+
+    const playlist = new SlashCommandBuilder()
+        .setName("playlist")
+        .setDescription("Plays a designated playlist from the link");
+
     client.application.commands.create(ping);
+    client.application.commands.create(help);
+    client.application.commands.create(playlist);
 });
 
 client.on(Events.InteractionCreate, interaction => {
     if(interaction.commandName === "ping") {
         interaction.reply("Pong!");
+    }
+
+    if(interaction.commandName === "help") {
+        interaction.reply("Fucking dumbass 🤣");
+    }
+
+    if(interaction.commandName === "help") {
+        interaction.reply("Playing playlist");
     }
 });
 
