d49480d4a9e0355a858a058354cc36157c8f66aa
CodyBense <codybense@gmail.com>
2024-12-12 22:21:57 -0500

    added ping command

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ const client = new Client({intents: []});
 client.once(Events.ClientReady, c => {
     console.log(`Logged in as ${c.user.username}`);
 
+    const ping = new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("Replies with ping!");
+
+    client.application.commands.create(ping);
+});
+
+client.on(Events.InteractionCreate, interaction => {
+    if(interaction.commandName === "ping") {
+        interaction.reply("Pong!");
+    }
 });
 
 
