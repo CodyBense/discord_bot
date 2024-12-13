fad27691e1d15d434623704de51a53b3d4a2ae84
CodyBense <codybense@gmail.com>
2024-12-12 22:15:29 -0500

    added login console log

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const {Client} = require('discord.js');
-const {token} = require('./config.json');
+const { Client, Events, SlashCommandBuilder } = require('discord.js');
+const { token } = require('./config.json');
 
 
 const client = new Client({intents: []});
+
+client.once(Events.ClientReady, c => {
+    console.log(`Logged in as ${c.user.username}`);
+
+});
+
 
 client.login(token);
